f3c8325b4cfc66feb39e15efd6c71fb1a18b08b9
Felipe Ribeiro <feliper.dev@gmail.com>
2024-11-17 14:20:30 -0300

    feat: Botões da tela inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,24 @@
+import HomeButton from "@/components/HomeButton";
+
 export default function Home() {
-  return <div className="h-full w-full bg-green-300"></div>;
+  return (
+    <main className="flex h-full w-full flex-col place-items-center bg-green-300 px-8">
+      <h1 className="my-8 text-center text-4xl">Poké-AI</h1>
+      <HomeButton
+        text="Iniciar"
+        navigation="/gamemode"
+        navigationParams={null}
+      />
+      <HomeButton
+        text="Como jogar"
+        navigation="/how-to-play"
+        navigationParams={null}
+      />
+      <HomeButton
+        text="Sobre a equipe"
+        navigation="/about"
+        navigationParams={null}
+      />
+    </main>
+  );
 }
