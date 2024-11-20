dccc36bb2d25bd82bc69ebd2070f5b7e948eaa3e
Daniel Marinho <70353676+D4NL18@users.noreply.github.com>
2024-11-20 17:53:33 -0300

    Revert "Feature/battle component"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,3 @@
-import PokemonInBattle from "@/components/PokemonInBattle";
-
 export default function Home() {
-  const Pikachu = {
-    nome: "Pikachu",
-    vida: 19,
-    type1: "Electric",
-    type2: "None",
-    imageUrl: "https://archives.bulbagarden.net/media/upload/thumb/4/4a/0025Pikachu.png/800px-0025Pikachu.png",
-    moveset: [
-      { name: "Thunderbolt", type: "Electric", damage: 40 },
-      { name: "Quick Attack", type: "Normal", damage: 30 },
-      { name: "Iron Tail", type: "Steel", damage: 50 },
-      { name: "Electro Ball", type: "Electric", damage: 60 },
-    ],
-  };
-
-  const Chimchar = {
-    nome: "Chimchar",
-    vida: 73,
-    type1: "Fire",
-    type2: "Fighting",
-    imageUrl: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/390.png",
-    moveset: [
-      { name: "Flamethrower", type: "Fire", damage: 40 },
-      { name: "Mach Punch", type: "Fighting", damage: 30 },
-      { name: "Scratch", type: "Normal", damage: 20 },
-      { name: "Fire Spin", type: "Fire", damage: 50 },
-    ],
-  };
-
-  return (
-    <div className="flex flex-col xl:flex-row items-center justify-evenly min-h-full bg-white">
-      <PokemonInBattle pokemon={Pikachu} />
-      <PokemonInBattle pokemon={Chimchar} />
-    </div>
-  );
+  return <div className="h-full w-full bg-green-300"></div>;
 }
--- src/components/PokedexLayout/index.tsx
+++ src/components/PokedexLayout/index.tsx
@@ -9,28 +9,28 @@ export default function PokedexLayout({
         <div className="h-7 w-6 border border-zinc-950"></div>
         <div className="h-7 w-6 border border-zinc-950"></div>
       </div>
-      <div className="h-[calc(100vh-1.75rem)] overflow-auto bg-red-700 px-10 py-5 md:pl-36 md:pr-12">
-        <div className="h-full w-full flex">{children}</div>
+      <div className="h-[calc(100vh-1.75rem)] overflow-auto bg-red-700 py-5 pl-36 pr-12">
+        <div className="h-full w-full">{children}</div>
       </div>
       {/* linha lateral */}
       <div className="fixed top-0 flex h-[calc(100vh-20.1rem)] w-7 flex-col items-center justify-end border border-zinc-950 border-b-transparent bg-red-700"></div>
 
       {/* bolinhas lateral */}
-      <div className="fixed top-0 flex h-full w-7 flex-col items-center justify-center border border-black border-b-transparent border-t-transparent md:h-[calc(100vh-10.8rem)] md:justify-end md:border-r-transparent">
+      <div className="fixed top-0 flex h-[calc(100vh-12rem)] w-7 flex-col items-center justify-end">
         <div className="my-3 h-4 w-4 rounded-full border border-zinc-950 bg-green-700"></div>
         <div className="my-3 h-4 w-4 rounded-full border border-zinc-950 bg-yellow-500"></div>
         <div className="my-3 h-4 w-4 rounded-full border border-zinc-950 bg-red-800"></div>
       </div>
 
       {/* quadrado */}
-      <div className="fixed bottom-0 hidden h-44 w-[7.72rem] items-start justify-center border border-zinc-950 border-y-transparent md:flex">
+      <div className="fixed bottom-0 flex h-44 w-[7.72rem] items-start justify-center border border-zinc-950 border-y-transparent">
         <div className="mt-4 flex h-20 w-20 items-center justify-start rounded-full border border-zinc-950 bg-sky-400">
           <div className="mb-6 ml-2 h-8 w-8 rounded-full bg-sky-200"></div>
         </div>
       </div>
 
       {/* triangulo linha inclinada */}
-      <div className="fixed bottom-40 left-[4.7rem] hidden h-44 w-[1px] rotate-[147deg] bg-zinc-950 md:block"></div>
+      <div className="fixed bottom-40 left-[4.7rem] h-44 w-[1px] rotate-[147deg] bg-zinc-950"></div>
     </>
   );
 }
