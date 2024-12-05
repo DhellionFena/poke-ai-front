351109b74cb3a736464762dfb64ceb6356b30ebc
Pedro Barros <82074537+pedrobarros01@users.noreply.github.com>
2024-12-05 15:23:01 -0300

    fix: colocando efeito do pokemon no congratulations
    Co-Authored-By: Antônio César <antonioazevedo.dev@gmail.com>

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['avatars.githubusercontent.com']
+    remotePatterns: [{
+      hostname: 'avatars.githubusercontent.com'
+    },
+    {
+      hostname: "oaidalleapiprodscus.blob.core.windows.net"
+    },
+    {
+      hostname: "archives.bulbagarden.net"
+    }
+  ]
   }
 };
 
--- src/app/congratulations/page.tsx
+++ src/app/congratulations/page.tsx
@@ -1,14 +1,17 @@
 "use client";
 
 import HomeButton from "@/components/HomeButton";
+import LoadingPokeBola from "@/components/Loading";
+import PokemonImage from "@/components/PokemonInBattle/PokemonImage";
 import { Pokemon } from "@/types/pokemonForm";
+import { tree } from "next/dist/build/templates/app-page";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
 export default function Congratulations() {
   const [vencedor, setVencedor] = useState<Pokemon | null>(null);
-
   useEffect(() => {
+      
     const vencedor = localStorage.getItem("VENCEDOR");
 
     const p1 = localStorage.getItem("PLAYER1");
@@ -19,17 +22,19 @@ export default function Congratulations() {
     } else if (vencedor === "PLAYER2" && p1 && p2) {
       setVencedor(JSON.parse(p2) as Pokemon);
     }
+    
   }, []);
-
   return (
-    <main className="flex h-full flex-col">
+    <main className="flex h-full flex-col items-center">
       <div className="flex w-fit flex-col self-center">
         <h1>PARABÉNS AO VENCEDOR</h1>
         <h2>{vencedor?.nome}</h2>
       </div>
 
-      <Image src={vencedor?.sprite as string} alt="Imagem Vencedor" height={500} width={500} />
-
+      <div className="w-1/2">
+        <PokemonImage image={vencedor?.sprite ? vencedor.sprite : "https://archives.bulbagarden.net/media/upload/thumb/4/4a/0025Pikachu.png/800px-0025Pikachu.png"} />
+      </div>
+      
       <HomeButton
         text="Próximo"
         navigation="/end-game"
