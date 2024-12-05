4e882e474bc0528809900e5477fceb515dcf6f4c
Felipe Ribeiro <feliper.dev@gmail.com>
2024-12-04 21:29:46 -0300

    fix: Removendo onClick desncesessário

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,9 +9,6 @@ export default function Home() {
       <HomeButton
         text="Iniciar"
         navigation="/gamemode"
-        onClick={() => {
-          console.log("aoba");
-        }}
         navigationParams={null}
       />
       <HomeButton
